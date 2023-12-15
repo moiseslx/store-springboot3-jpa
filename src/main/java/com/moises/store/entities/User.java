a85c476424d6d427c544ed6e222488118a52a56b
Moises Almeida <moisesalmeida.dev@gmail.com>
2023-12-15 19:41:40 -0400

    H2 database, test profile, JPA

--- src/main/java/com/moises/store/entities/User.java
+++ src/main/java/com/moises/store/entities/User.java
@@ -1,5 +1,6 @@
 package com.moises.store.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +11,14 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
