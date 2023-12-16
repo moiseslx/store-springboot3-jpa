80350acb34aa3fbcceed8157a5f96858d4ed52e0
Moises Almeida <moisesalmeida.dev@gmail.com>
2023-12-15 20:03:49 -0400

    Service layer, component registration

--- src/main/java/com/moises/store/resources/UserResource.java
+++ src/main/java/com/moises/store/resources/UserResource.java
@@ -1,17 +1,32 @@
 package com.moises.store.resources;
 
 import com.moises.store.entities.User;
+import com.moises.store.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        return ResponseEntity.ok().body(new User(1L, "Moises Almeida", "LbYnI@example.com", "123456789", "123456"));
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/moises/store/services/UserService.java
@@ -0,0 +1,25 @@
+package com.moises.store.services;
+
+import com.moises.store.entities.User;
+import com.moises.store.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
