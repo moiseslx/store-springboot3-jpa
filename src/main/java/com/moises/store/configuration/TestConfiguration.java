1576c93fba7b9352852e7ef7db0de7918b02af96
Moises Almeida <moisesalmeida.dev@gmail.com>
2023-12-15 20:52:58 -0400

    OrderStatus enum

--- src/main/java/com/moises/store/configuration/TestConfiguration.java
+++ src/main/java/com/moises/store/configuration/TestConfiguration.java
@@ -2,6 +2,7 @@ package com.moises.store.configuration;
 
 import com.moises.store.entities.Order;
 import com.moises.store.entities.User;
+import com.moises.store.entities.enums.OrderStatus;
 import com.moises.store.repositories.OrderRepository;
 import com.moises.store.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfiguration implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.WAITING_PAYMENT);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.SHIPPED);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/moises/store/entities/Order.java
+++ src/main/java/com/moises/store/entities/Order.java
@@ -1,6 +1,7 @@
 package com.moises.store.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.moises.store.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,8 +27,28 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
+    //CUSTOM CONSTRUCTOR
+    public Order(Long id, Instant moment, User client, OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+        setOrderStatus(status);
+    }
+
+    //CUSTOM GETTER AND SETTER METHOD
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
 }
