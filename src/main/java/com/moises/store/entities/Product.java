49be8309c359cfa909f6e7aa8093905c42707a71
Moises Almeida <moisesalmeida.dev@gmail.com>
2023-12-17 13:36:09 -0400

    Product-OrderItem one-to-many association

--- src/main/java/com/moises/store/entities/Product.java
+++ src/main/java/com/moises/store/entities/Product.java
@@ -1,9 +1,8 @@
 package com.moises.store.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -12,7 +11,6 @@ import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
@@ -21,15 +19,24 @@ public class Product implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
     private Long id;
+    @Getter @Setter
     private String name;
+    @Getter @Setter
     private String description;
+    @Getter @Setter
     private Double price;
+    @Getter @Setter
     private String imageUrl;
 
+    @Getter
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private List<Category> categories = new ArrayList<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private List<OrderItem> items = new ArrayList<>();
 
     // CUSTOM CONSTRUCTOR
     public Product(Long id, String name, String description, Double price, String imageUrl) {
@@ -45,5 +52,17 @@ public class Product implements Serializable {
         if (!this.categories.contains(category)) {
             this.categories.add(category);
         }
+    }
+
+    // Adds an item to the list of items if it is not already present.
+    @JsonIgnore
+    public List<Order> getOrders() {
+        List<Order> list = new ArrayList<>();
+        for (OrderItem x : items) {
+            if (!list.contains(x.getOrder())) {
+                list.add(x.getOrder());
+            }
+        }
+        return list;
     }
 }
